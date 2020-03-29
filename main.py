22c7c1b38760e476f575b16beaf11a1e621874d2
Jonathan Ko <jkoj25@gmail.com>
2020-03-28 22:58:27 -0700

    added after_id, arduino connectivity

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import tkinter as tk
 from tkinter import *
 import time
 import pyautogui
+import serial
+import numpy
 
-running = False
 count = 0
 s = 300
 r = s//6
+after_id = None
 
 height = 200
 width = 500
@@ -16,7 +18,7 @@ offset = 100
 emotions = ["anger", "joy", "surprise", "sorrow", "neutral"]
 colors = ["red", "pink", "yellow", "blue", "gray"]
 bars = [0 for i in range(5)]
-firsttime = True;
+firsttime = True
 
 def bargraphsetup():
     c.create_text(50, 280, text = "Anger")
@@ -32,6 +34,15 @@ def bargraphsetup():
     c.create_rectangle(545, 230, 575, 260, fill = colors[4])
 
     c.create_text(offset + 200, offset/2, text = "Live-Updating Mood Tracker")
+    
+    #c.create_line(offset, height + offset, offset, offset)
+    #c.create_line(offset, height + offset, width + offset, height + offset)
+    #create_text(1.5*offset + width//2, height + 1.5*offset, text = 'Emotions')
+    #c.create_text(.3*offset, offset + .5*height, text = 'Percent')
+
+    #for i in range(5):
+    #    c.create_text(.8*offset, height + offset - height//4*i, text = '' + str(100/4*i) + '%')
+    #    c.create_text((2*i + 1.5)*width//10 + offset, height + 1.2 * offset, text = emotions[i])
 
 def bargraph(data):
     global firsttime
@@ -50,32 +61,37 @@ def bargraph(data):
         cumulative_length += length
         bars[i] = c.create_rectangle(x1,y1,x2,y2,fill = colors[i])
 
+    #for i in range(5):
+    #    h = height * data[i]
+    #    bars[i] = c.create_rectangle((2*i + 1)*width//10 + offset, height + offset, (2*i+2) * width//10 + offset, height + offset - h, fill = colors[i], outline = '')
+
 def start():
     global running
     button['text'] = 'Stop'
     button['command'] = stop
-    running = True
     print("Start")
     takeScreenshot()
 
 def takeScreenshot():
     global count
-    if (running):
-        myScreenshot = pyautogui.screenshot()
-        myScreenshot.save('C:/Users/natha/Downloads/screenshot.png')
-        count = count +1
-        data = detect_faces('screenshot.png')
-        print(data)
-        bargraph(data)
-        
-        #t.after(30000, takeScreenshot())
+    global after_id
+    myScreenshot = pyautogui.screenshot()
+    myScreenshot.save('C:/Users/Jonathan Ko/Documents/Hackathons/LAHacks/screenshot.png')
+    count = count +1
+    data = detect_faces('screenshot.png')
+    print(data)
+    bargraph(data)  
+    sendNumber(data)
+    after_id = t.after(500, takeScreenshot)
 
 def stop():
-    global running
-    print ("Stop")
-    button['text'] = 'Start'
-    button['command'] = start
-    running = False
+    global after_id
+    if after_id:
+        t.after_cancel(after_id)
+        after_id = None
+        print ("Stop")
+        button['text'] = 'Start'
+        button['command'] = start
 
 
 def detect_faces(path):
@@ -111,7 +127,13 @@ def detect_faces(path):
     total = 0
     for val in values:
         total += val
+
+    if total == 0:
+        total = 1
+
     values = [values[i] / total for i in range(len(values))]
+
+   
 
     if response.error.message:
         raise Exception(
@@ -120,6 +142,14 @@ def detect_faces(path):
                 response.error.message))
     return values
 
+def sendNumber(data):
+    total = 0
+    for i in range(len(data)):
+        if data[i] == numpy.amax(data):
+            total = i + 1
+            break
+    arduino.write(str.encode(str(total)))
+    return
 
 t = tk.Tk()
 t.geometry(str(width + 2 * offset)+'x'+str(height + offset*2))  
